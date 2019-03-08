e963e55b940d6d212910e4b08f447c7ef36346ee
Anders Hovmöller <boxed@killingar.net>
2019-03-08 21:04:46 +0100

    Initial Commit

---
+++ ForumChecker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ForumChecker
+//
+//  Created by Anders Hovmöller on 2019-03-08.
+//  Copyright © 2019 Anders Hovmöller. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
